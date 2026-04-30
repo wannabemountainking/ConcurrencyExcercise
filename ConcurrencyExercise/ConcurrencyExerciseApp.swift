725b8a64987f9fd013d2702db02db55d7dc97394
wannabemountainking <kdy0096@gmail.com>
2026-04-30 09:37:19 +0900

    Initial Commit

---
+++ ConcurrencyExercise/ConcurrencyExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyExerciseApp.swift
+//  ConcurrencyExercise
+//
+//  Created by YoonieMac on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
